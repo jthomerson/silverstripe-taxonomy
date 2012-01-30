4d80e0207dc40eb56eaf13c5e83b11e7b9f00b3e
Jeremy Thomerson <jeremy@thomersonfamily.com>
2012-01-30 18:33:42 +0000

    Initial commit - _config.php file

---
+++ _config.php
@@ -0,0 +1,8 @@
+<?php
+
+/* The silverstripe-taxonomy module provides a way of giving a secondary
+ * categorization to content by applying vocabulary terms (or "tags") to nodes.
+ *
+ * This config file currently contains no configuration, but must exist for
+ * SilverStripe to recognize this folder as a module.
+ */
